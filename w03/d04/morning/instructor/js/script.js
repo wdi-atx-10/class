62b3f5b23d6ecd4fce5e08d546e461b5c221bda7
Britney Jo <me@britneyjo.com>
2017-03-23 12:26:32 -0500

    add firstname and last name functionality

--- w03/d04/morning/instructor/js/script.js
+++ w03/d04/morning/instructor/js/script.js
@@ -11,16 +11,29 @@ function getRandomJoke(){
   var apiUrl = 'http://api.icndb.com/jokes/random';
   var data = {};
 
+  var firstName = $('#first-name').val();
+  var lastName = $('#last-name').val();
+
+  if (firstName.length > 0){
+    data.firstName = firstName
+  }
+
+  if (lastName.length > 0){
+    data.lastName = lastName
+  }
+
+  // console.log('form data', data)
+
   $.ajax({
     url: apiUrl,
     data: data
   })
   .done(function(response){
-    console.log('response', response);
+    // console.log('response', response);
     var oldJoke = $('#featured-jokes').html();
     $('#previous-jokes').prepend(oldJoke);
     $('#featured-jokes').html('');
-    
+
     var joke = response.value.joke;
     $('#featured-jokes').append('<li>' + joke + '</li>');
   })
