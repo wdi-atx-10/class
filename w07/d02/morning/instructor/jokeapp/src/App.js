76d03f7914743d61ccad988a3e718169e9383080
Britney Jo <me@britneyjo.com>
2017-04-18 10:46:25 -0500

    create Joke and JokeList with props

--- w07/d02/morning/instructor/jokeapp/src/App.js
+++ w07/d02/morning/instructor/jokeapp/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+class JokeList extends Component {
+  render(){
+    return(
+      <div>
+        <Joke text="joke1"/>
+        <Joke text="joke 2"/>
+      </div>
+    )
+  }
+}
+
+
+class Joke extends Component {
+  render(){
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h3>{this.props.text}</h3>
       </div>
     );
   }
 }
 
-export default App;
+
+export default JokeList;
