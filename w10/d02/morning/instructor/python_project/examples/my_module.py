c408ad9a4abcaef0bf4c5edbd96c3a21cad8ae9b
dirkdunn <dirkdunn1@gmail.com>
2017-05-09 15:01:58 -0500

    python project

---
+++ w10/d02/morning/instructor/python_project/examples/my_module.py
@@ -0,0 +1,11 @@
+
+
+def hello_world():
+    print __name__
+    print "hello world from my_module.py"
+
+
+class Animal:
+    def __init__(self):
+        self.furcolor = 'red'
+        self.has_a_tail = True
